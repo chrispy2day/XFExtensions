0e2e5ab7ac36895164644b3524ee19a45359076b
Chris Plowman <chris@im.com>
2015-07-10 18:04:13 -0700

    Started creation of a sample Xamarin.Forms app to demo the extensions

---
+++ XFExtensions.Samples/XFExtensions.Samples.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using XFExtensions.Samples.WinPhone.Resources;
+
+namespace XFExtensions.Samples.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
