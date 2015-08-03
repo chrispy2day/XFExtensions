1ad0e70122bd65adbe3e24010c5ceedaaf846a31
Chris Plowman <chris@im.com>
2015-08-03 11:43:00 -0700

    Started creating a test page for the MetaMediaService

---
+++ XFExtensions.Samples/XFExtensions.Samples/ViewModels/MediaVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using MetaMediaPlugin;
+using Xamarin.Forms;
+
+namespace XFExtensions.Samples.ViewModels
+{
+    public class MediaVM
+    {
+        public ICommand ChoosePhotoCommand { get; private set; }
+
+        public MediaVM()
+        {
+            ChoosePhotoCommand = new Command(
+                async (_) => await MetaMedia.Current.PickPhotoAsync(), 
+                o => MetaMedia.Current.IsPickPhotoSupported);
+        }
+    }
+}
---
+++ XFExtensions.Samples/XFExtensions.Samples/Views/MediaPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace XFExtensions.Samples.Views
+{
+    public partial class MediaPage : ContentPage
+    {
+        public MediaPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
